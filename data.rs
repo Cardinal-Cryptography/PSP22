b8d93b9de0dc800d7092ab1576fcc54d6f0a3b03
Michal Handzlik <4hansu@gmail.com>
2023-10-02 16:56:07 +0200

    Move token operations logic inside PSP22Data struct

--- data.rs
+++ data.rs
@@ -1,4 +1,14 @@
-use ink::{primitives::AccountId, storage::Mapping};
+use crate::PSP22Error;
+use ink::{
+    prelude::{vec, vec::Vec},
+    primitives::AccountId,
+    storage::Mapping,
+};
+
+pub enum PSP22Event {
+    Transfer(Option<AccountId>, Option<AccountId>, u128),
+    Approval(AccountId, AccountId, u128),
+}
 
 #[ink::storage_item]
 #[derive(Debug, Default)]
@@ -17,5 +27,146 @@ impl PSP22Data {
         };
         data.balances.insert(creator, &supply);
         data
+    }
+
+    pub fn total_supply(&self) -> u128 {
+        self.total_supply
+    }
+
+    pub fn balance_of(&self, owner: AccountId) -> u128 {
+        self.balances.get(owner).unwrap_or_default()
+    }
+
+    pub fn allowance(&self, owner: AccountId, spender: AccountId) -> u128 {
+        self.allowances.get((owner, spender)).unwrap_or_default()
+    }
+
+    pub fn transfer(
+        &mut self,
+        caller: AccountId,
+        to: AccountId,
+        value: u128,
+        _data: Vec<u8>,
+    ) -> Result<Vec<PSP22Event>, PSP22Error> {
+        if caller == to || value == 0 {
+            return Ok(vec![]);
+        }
+        let from_balance = self.balance_of(caller);
+        if from_balance < value {
+            return Err(PSP22Error::InsufficientBalance);
+        }
+
+        if from_balance == value {
+            self.balances.remove(caller);
+        } else {
+            self.balances
+                .insert(caller, &(from_balance.saturating_sub(value)));
+        }
+        let to_balance = self.balance_of(to);
+        // Total supply is limited by u128.MAX so no overflow is possible
+        self.balances
+            .insert(to, &(to_balance.saturating_add(value)));
+        Ok(vec![PSP22Event::Transfer(Some(caller), Some(to), value)])
+    }
+
+    pub fn transfer_from(
+        &mut self,
+        caller: AccountId,
+        from: AccountId,
+        to: AccountId,
+        value: u128,
+        data: Vec<u8>,
+    ) -> Result<Vec<PSP22Event>, PSP22Error> {
+        if from == to || value == 0 {
+            return Ok(vec![]);
+        }
+        if caller == from {
+            return self.transfer(caller, to, value, data);
+        }
+
+        let allowance = self.allowance(from, caller);
+        if allowance < value {
+            return Err(PSP22Error::InsufficientAllowance);
+        }
+        let from_balance = self.balance_of(from);
+        if from_balance < value {
+            return Err(PSP22Error::InsufficientBalance);
+        }
+
+        if allowance == value {
+            self.allowances.remove((from, caller));
+        } else {
+            self.allowances
+                .insert((from, caller), &(allowance.saturating_sub(value)));
+        }
+
+        if from_balance == value {
+            self.balances.remove(from);
+        } else {
+            self.balances
+                .insert(from, &(from_balance.saturating_sub(value)));
+        }
+        let to_balance = self.balance_of(to);
+        // Total supply is limited by u128.MAX so no overflow is possible
+        self.balances
+            .insert(to, &(to_balance.saturating_add(value)));
+        Ok(vec![
+            PSP22Event::Approval(from, caller, allowance.saturating_sub(value)),
+            PSP22Event::Transfer(Some(from), Some(to), value),
+        ])
+    }
+
+    pub fn approve(
+        &mut self,
+        owner: AccountId,
+        spender: AccountId,
+        value: u128,
+    ) -> Result<Vec<PSP22Event>, PSP22Error> {
+        if owner == spender {
+            return Ok(vec![]);
+        }
+        if value == 0 {
+            self.allowances.remove((owner, spender));
+        } else {
+            self.allowances.insert((owner, spender), &value);
+        }
+        Ok(vec![PSP22Event::Approval(owner, spender, value)])
+    }
+
+    pub fn increase_allowance(
+        &mut self,
+        owner: AccountId,
+        spender: AccountId,
+        delta_value: u128,
+    ) -> Result<Vec<PSP22Event>, PSP22Error> {
+        if owner == spender || delta_value == 0 {
+            return Ok(vec![]);
+        }
+        let allowance = self.allowance(owner, spender);
+        let amount = allowance.saturating_add(delta_value);
+        self.allowances.insert((owner, spender), &amount);
+        Ok(vec![PSP22Event::Approval(owner, spender, amount)])
+    }
+
+    pub fn decrease_allowance(
+        &mut self,
+        owner: AccountId,
+        spender: AccountId,
+        delta_value: u128,
+    ) -> Result<Vec<PSP22Event>, PSP22Error> {
+        if owner == spender || delta_value == 0 {
+            return Ok(vec![]);
+        }
+        let allowance = self.allowance(owner, spender);
+        if allowance < delta_value {
+            return Err(PSP22Error::InsufficientAllowance);
+        }
+        let amount = allowance.saturating_sub(delta_value);
+        if amount == 0 {
+            self.allowances.remove((owner, spender));
+        } else {
+            self.allowances.insert((owner, spender), &amount);
+        }
+        Ok(vec![PSP22Event::Approval(owner, spender, amount)])
     }
 }
