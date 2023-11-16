9453423723bcded16dbf45cae623fbf9284ed484
Edinyanga O. Ottoho <elzucky@gmail.com>
2023-11-16 14:41:22 +0100

    Create lib.rs
    A minimal example for implementing the PSP22 using your own logic.

---
+++ examples/lib.rs
@@ -0,0 +1,178 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[cfg(feature = "contract")]
+#[ink::contract]
+mod token {
+    use psp22::{PSP22Data, PSP22Error, PSP22Event, PSP22Metadata, PSP22, PSP22Mintable};
+    use psp22::PSP22Error::Custom;
+    use ink::prelude::{string::String, vec::Vec};
+
+    #[ink(storage)]
+    pub struct Token {
+        data: PSP22Data,
+        name: Option<String>,
+        symbol: Option<String>,
+        decimals: u8,
+        owner: Option<AccountId>
+    }
+
+    impl Token {
+        #[ink(constructor)]
+        pub fn new(
+            name: Option<String>,
+            symbol: Option<String>,
+            decimals: u8,
+            owner: AccountId
+        ) -> Self {
+            Self {
+                data: PSP22Data::new(0, Self::env().caller()),
+                name,
+                symbol,
+                decimals,
+                owner: Some(owner)
+            }
+        }
+
+        fn emit_events(&self, events: Vec<PSP22Event>) {
+            for event in events {
+                match event {
+                    PSP22Event::Transfer { from, to, value } => {
+                        self.env().emit_event(Transfer { from, to, value })
+                    }
+                    PSP22Event::Approval {
+                        owner,
+                        spender,
+                        amount,
+                    } => self.env().emit_event(Approval {
+                        owner,
+                        spender,
+                        amount,
+                    }),
+                }
+            }
+        }
+    }
+
+    #[ink(event)]
+    pub struct Approval {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        spender: AccountId,
+        amount: u128,
+    }
+
+    #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: Option<AccountId>,
+        #[ink(topic)]
+        to: Option<AccountId>,
+        value: u128,
+    }
+
+    impl PSP22 for Token {
+        #[ink(message)]
+        fn total_supply(&self) -> u128 {
+            self.data.total_supply()
+        }
+
+        #[ink(message)]
+        fn balance_of(&self, owner: AccountId) -> u128 {
+            self.data.balance_of(owner)
+        }
+
+        #[ink(message)]
+        fn allowance(&self, owner: AccountId, spender: AccountId) -> u128 {
+            self.data.allowance(owner, spender)
+        }
+
+        #[ink(message)]
+        fn transfer(
+            &mut self,
+            to: AccountId,
+            value: u128,
+            _data: Vec<u8>,
+        ) -> Result<(), PSP22Error> {
+            let events = self.data.transfer(self.env().caller(), to, value)?;
+            self.emit_events(events);
+            Ok(())
+        }
+
+        #[ink(message)]
+        fn transfer_from(
+            &mut self,
+            from: AccountId,
+            to: AccountId,
+            value: u128,
+            _data: Vec<u8>,
+        ) -> Result<(), PSP22Error> {
+            let events = self
+                .data
+                .transfer_from(self.env().caller(), from, to, value)?;
+            self.emit_events(events);
+            Ok(())
+        }
+
+        #[ink(message)]
+        fn approve(&mut self, spender: AccountId, value: u128) -> Result<(), PSP22Error> {
+            let events = self.data.approve(self.env().caller(), spender, value)?;
+            self.emit_events(events);
+            Ok(())
+        }
+
+        #[ink(message)]
+        fn increase_allowance(
+            &mut self,
+            spender: AccountId,
+            delta_value: u128,
+        ) -> Result<(), PSP22Error> {
+            let events = self
+                .data
+                .increase_allowance(self.env().caller(), spender, delta_value)?;
+            self.emit_events(events);
+            Ok(())
+        }
+
+        #[ink(message)]
+        fn decrease_allowance(
+            &mut self,
+            spender: AccountId,
+            delta_value: u128,
+        ) -> Result<(), PSP22Error> {
+            let events = self
+                .data
+                .decrease_allowance(self.env().caller(), spender, delta_value)?;
+            self.emit_events(events);
+            Ok(())
+        }
+    }
+
+    impl PSP22Mintable for Token {
+        #[ink(message)]
+        fn mint(&mut self, account: AccountId, amount: Balance) -> Result<(), PSP22Error> {
+            if self.env().caller() != self.owner.unwrap() {
+                return Err(Custom(String::from("OnlyOwnerAllowed")));
+            }
+            let events = self.data.mint(account, amount)?;
+            self.emit_events(events);
+            Ok(())
+        }
+    }
+
+    impl PSP22Metadata for Token {
+        #[ink(message)]
+        fn token_name(&self) -> Option<String> {
+            self.name.clone()
+        }
+        #[ink(message)]
+        fn token_symbol(&self) -> Option<String> {
+            self.symbol.clone()
+        }
+        #[ink(message)]
+        fn token_decimals(&self) -> u8 {
+            self.decimals
+        }
+    }
+    
+}
